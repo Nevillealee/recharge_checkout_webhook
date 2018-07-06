b4a0ef4363088f201fa550081f45acc6865d53a3
Nevillealee <nevillealee@gmail.com>
2018-07-06 10:54:48 -0700

    rake webhook tasks

--- app/models/simple_webhook.rb
+++ app/models/simple_webhook.rb
@@ -2,18 +2,23 @@ module SimpleWebhook
   class RechargeInfo
     def initialize
       @my_header = {
-          "X-Recharge-Access-Token" => ENV['RECHARGE_STAGING_TOKEN']
+          "X-Recharge-Access-Token" => ENV['RECHARGE_STAGING_TOKEN'],
+          "content-type" => 'application/json'
       }
     end
 
     def list_webhooks
       #GET /webhooks/<id>
       webhooks = HTTParty.get("https://api.rechargeapps.com/webhooks", :headers => @my_header)
-      puts webhooks.body.inspect
+      # file = File.open(Rails.root.join('log/rake.log'), File::WRONLY | File::APPEND | File::CREAT)
+      # logger = Logger.new(file)
+      # logger.datetime_format = "%Y-%m-%d %H:%M"
+      # logger.info("Heres my logger output")
+      # logger.info(webhooks.body.inspect)
     end
 
     def create_webhook(hook, callback)
-      
+
       # POST /webhooks
       temp_hash = { "webhook" => {"address" => callback, "topic" => hook} }
       body = temp_hash.to_json
@@ -23,7 +28,6 @@ module SimpleWebhook
         :headers => @my_header,
         :body => body,
         :timeout => 80,
-        :headers => {"content-type" => 'application/json'}
       )
       puts new_webhook.inspect
     end
